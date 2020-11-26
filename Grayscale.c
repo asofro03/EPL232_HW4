7b479252f58df1761db9d0fa96170bb6e571b374
Antria <aargyr01@ucy.ac.cy>
2020-11-26 02:55:26 +0200

    bruh

--- Grayscale.c
+++ Grayscale.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 
-grayimage tograyscale(BITMAPINFOHEADER *img)
+BITMAPINFOHEADER tograyscale(BITMAPINFOHEADER *img)
 {
-   unsigned int x, y;
-   grayimage timg;
+    int x, y;
+   BITMAPINFOHEADER *timg;
    double rc, gc, bc, l;
    unsigned int ofs;
  
@@ -18,10 +18,10 @@ grayimage tograyscale(BITMAPINFOHEADER *img)
       for(y=0; y < img->biHeight; y++)
       {
         ofs = (y * img->biWidth) + x;
-        rc = (double) img->buf[ofs][0];
-        gc = (double) img->buf[ofs][1];
-        bc = (double) img->buf[ofs][2];
-        l = 0.299*rc + 0.587*gc + 0.114*bc;
+        rc = (double) img->buf[ofs][y];
+        gc = (double) img->buf[ofs][y+1];
+        bc = (double) img->buf[ofs][y+2];
+        (int) l = 0.299*rc + 0.587*gc + 0.114*bc;
         timg->buf[ofs][0] = (luminance) (l+0.5);
       }
    }
