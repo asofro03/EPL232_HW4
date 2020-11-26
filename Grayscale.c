39782e0b229fa6a41bf64077662f2b8d2b158499
Antria <aargyr01@ucy.ac.cy>
2020-11-26 02:46:43 +0200

    fixed one method kinda

--- Grayscale.c
+++ Grayscale.c
@@ -3,34 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-grayimage alloc_grayimg(unsigned int width, unsigned int height)
-{
-     grayimage img;
-     img = malloc(sizeof(grayimage_t));
-     img->buf = malloc(width *height*sizeof(pixel1));
-     img->width = width;
-     img->height = height;
-     return img;
-}
 
-grayimage tograyscale(IMAGE *img)
+grayimage tograyscale(BITMAPINFOHEADER *img)
 {
    unsigned int x, y;
    grayimage timg;
    double rc, gc, bc, l;
    unsigned int ofs;
  
-   timg = alloc_grayimg(img->width, img->height);
+   timg = alloc_grayimg(img->biWidth, img->biHeight);
  
-   for(x=0; x < img->width; x++)
+   for(x=0; x < img->biWidth; x++)
    {
-      for(y=0; y < img->height; y++)
+      for(y=0; y < img->biHeight; y++)
       {
-        ofs = (y * img->width) + x;
+        ofs = (y * img->biWidth) + x;
         rc = (double) img->buf[ofs][0];
         gc = (double) img->buf[ofs][1];
         bc = (double) img->buf[ofs][2];
-        l = 0.2126*rc + 0.7152*gc + 0.0722*bc;
+        l = 0.299*rc + 0.587*gc + 0.114*bc;
         timg->buf[ofs][0] = (luminance) (l+0.5);
       }
    }
