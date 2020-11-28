c3423980b96cd15d198bd387d829fdc01522f27c
Antria <aargyr01@ucy.ac.cy>
2020-11-28 19:42:03 +0200

    i shall fix this

--- Grayscale.c
+++ Grayscale.c
@@ -4,26 +4,44 @@
 #include <string.h>
 
 
-BITMAPINFOHEADER tograyscale(BITMAPINFOHEADER *img)
+int main()
 {
-    int x, y;
-   BITMAPINFOHEADER *timg;
-   double rc, gc, bc, l;
-   unsigned int ofs;
- 
-   timg = alloc_grayimg(img->biWidth, img->biHeight);
- 
-   for(x=0; x < img->biWidth; x++)
-   {
-      for(y=0; y < img->biHeight; y++)
-      {
-        ofs = (y * img->biWidth) + x;
-        rc = (double) img->buf[ofs][y];
-        gc = (double) img->buf[ofs][y+1];
-        bc = (double) img->buf[ofs][y+2];
-        (int) l = 0.299*rc + 0.587*gc + 0.114*bc;
-        timg->buf[ofs][0] = (luminance) (l+0.5);
-      }
-   }
-   return timg;
+    FILE *fIn = fopen("tiger.bmp", "rb");
+    FILE *fOut = fopen("tiger_gray.bmp", "wb");
+    if (!fIn || !fOut)
+    {
+        printf("File error.\n");
+        return 0;
+    }
+
+    unsigned char header[54];
+    fread(header, sizeof(unsigned char), 54, fIn);
+    fwrite(header, sizeof(unsigned char), 54, fOut);
+
+    int width = *(int*)&header[18];
+    int height = abs(*(int*)&header[22]);
+    int stride = (width * 3 + 3) & ~3;
+    int padding = stride - width * 3;
+
+    printf("width: %d (%d)\n", width, width * 3);
+    printf("height: %d\n", height);
+    printf("stride: %d\n", stride);
+    printf("padding: %d\n", padding);
+
+    unsigned char pixel[3];
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            fread(pixel, 3, 1, fIn);
+            unsigned char gray = pixel[0] * 0.3 + pixel[1] * 0.58 + pixel[2] * 0.11;
+            memset(pixel, gray, sizeof(pixel));
+            fwrite(&pixel, 3, 1, fOut);
+        }
+        fread(pixel, padding, 1, fIn);
+        fwrite(pixel, padding, 1, fOut);
+    }
+    fclose(fOut);
+    fclose(fIn);
+    return 0;
 }
