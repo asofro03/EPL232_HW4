2a5c905e41590374b701b3b0cd46a7683a8cbf07
Antria <aargyr01@ucy.ac.cy>
2020-11-30 00:44:35 +0200

    comments

--- Grayscale.c
+++ Grayscale.c
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
  
-
+/**
+ * @brief this method turns the colors from a rainbow color to grayscale onr(black and white)
+ * @param rgb 
+ *  
+ */
 void grayscaleEffect(unsigned char *rgb){
 	float value = 0.299*rgb[0] + 0.587*rgb[1] + 0.114*rgb[2];
 	int grayscale = round(value);
@@ -22,7 +26,7 @@ void BmpEffects(char *filename){
 	BITMAPINFOHEADER *infoheader = newImage(fp);
 	
 	int padding = paddingBytes(infoheader);
-	int Headerbytes = fileheader->offbits;
+	int Headerbytes = fileheader->biBitCount;
 	int Databytes = fileheader->biSizeImage - Headerbytes;
 	int widthbytes = infoheader->biWidth*3 ;
 
