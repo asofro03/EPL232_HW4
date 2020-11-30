2668d8b1f0976ad3790c26239c4f75d625c424fc
asofro03 <asofro03@ucy.ac.cy>
2020-11-30 06:16:22 +0200

    comments in string to image.h

--- StringToImage.h
+++ StringToImage.h
@@ -1,3 +1,9 @@
+/** @file StringToImage.h
+ *  @brief Creates images from strings.
+ * 
+ *  @bug Incompleted.
+ */
+
 #ifndef STRINGTOIMAGE_h
 #define STRINGTOIMAGE_h
 #include "list.h"
@@ -8,7 +14,31 @@ typedef struct{
     byte blue;
 }PIXEL;
 
-IMAGE stringToImage(char *image, char *filename);
+/**
+ * @brief Creates a pixel by using 3 bytes from data.
+ * 
+ * @param r Red byte.
+ * @param g Green byte.
+ * @param b Blue byte.
+ * @return PIXEL 
+ */
+PIXEL newPixel(byte r, byte g, byte b);
+
+/**
+ * @brief Creates an image from a text.
+ * 
+ * @param picture The image prototype for the new image.
+ * @param filename The filename that the text is in.
+ * @return IMAGE* An image from text.
+ */
+IMAGE *stringToImage(char *picture, char *filename);
+
+/**
+ * @brief Creates text from an image.
+ * 
+ * @param picture The picture the text is going to be created from.
+ */
+void imageToString(char *picture);
 
 /**
  * @brief creates an array with integers and then 'suffles' it
