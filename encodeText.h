6d87c7e873d92b8b1c66bc1267ee0de822646093
Antria <aargyr01@ucy.ac.cy>
2020-11-30 06:15:42 +0200

    encodeTExt.h

---
+++ encodeText.h
@@ -0,0 +1,14 @@
+#ifndef ENCODETEXT_H
+#define ENCODETEXT_H
+
+
+/**
+ * @brief creates an array with integers and then 'suffles' it
+ * @param N
+ * @param systemkey
+ */
+int *createPermutationFunction(int N, unsigned int systemkey);
+
+
+
+#endif
