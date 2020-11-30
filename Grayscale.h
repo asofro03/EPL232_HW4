80f94f047f27fd028e663c65c13f7998eaa7818b
Antria <aargyr01@ucy.ac.cy>
2020-11-30 06:04:12 +0200

    finiched

--- Grayscale.h
+++ Grayscale.h
@@ -1,7 +1,7 @@
 /** @file Grayscale
  *  @brief turns a colored image to a grayscale one
  *  @author Antria Argyrou
- *  @bug not known 
+ *  @bug does not compile  
  */
 
 #ifndef GRAYSCALE_H
@@ -9,17 +9,27 @@
 
 /**
  * @brief this method turns the colors from a rainbow color to grayscale onr(black and white)
- * @param rgb 
+ * @param rgb array
  *  
  */
 void grayscaleEffect(unsigned char *rgb);
 
 /**
- * @brief opens a file which is the image , seperates the needed and then calls the grayscaleEffect method 
- *        to chande the colors
+ * @brief thats an extra method that changes the image to a pinkscale
+ * @param rgb array
+ * 
+ */ 
+void pinkscaleEffect(unsigned char *rgb);
+
+/**
+ * @brief this method is where actually the change happends it takes the 
+ *		 image and change the rgb 3 by 3 by calling the grayscale effect method 
+ *        
  * @param filename is the image
+ * @param effect the 1 is for the homework and the 2 is the extra method
  * 
  */
-void bmpGr(char *filename);
+
+void bmpGr(char *filename, int effect);
 
 #endif
