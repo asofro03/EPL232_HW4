b531757443edfed071d3cd5d134b2a1eba518dff
asofro03 <asofro03@ucy.ac.cy>
2020-11-30 06:34:16 +0200

    client creation

---
+++ bmpSteganography.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "list.h"
+#include "Grayscale.h"
+#include "Stegano.h"
+#include "  "
+#include "StringToImage.h"
+
+//client
+void main(int argc, char *argv[]){
+
+    if(argc<2){
+         printf("Not enough arguments\n");
+        return 0;}  
+    
+    if(strcmp(argv[1], "-list")==0){
+        int c;
+        for(c=2; c<argc; c++){
+            IMAGE *image = newImage(argv[c]);
+            printList(image);
+            printf("***************************************************************************\n");
+  }     
+        
+    }
+
+
+}
